a50a1d6e2f65af06e718ff48fdfaa5f2752d8221
Ian Ndegwa <iansky3311@gmail.com>
2024-02-23 15:05:11 +0300

    completed assignment

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# name input
+name = input("What is your name: ")
+
+# age input
+age = int(input("Enter your age: "))
+
+# location input
+location = input("Where are you located: ")
+
+# song and personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
